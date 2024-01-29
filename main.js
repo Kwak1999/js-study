8de75b70164eade31b39dfd56883d78c83aa5ac4
KHB2937 <gmlqja7913@naver.com>
2024-01-30 01:19:14 +0900

    js기본 정리 시작

---
+++ main.js
@@ -0,0 +1,16 @@
+let fruits = ['Apple', 'Banana', 'Cherry'];
+let user = {
+    name: 'HEROPY',
+    age: 85
+};
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+console.log(new Date('2021-01-30').getDay());
+console.log(new Date('2021-01-31').getDay());
+console.log(new Date('2021-02-01').getDay());
+
+console.log(user);
+console.log(user.age);
